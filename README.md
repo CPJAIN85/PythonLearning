a42f91a65cfbd695886e9d7a17647f3560a230f2
CPJAIN85 <chandujain30@gmail.com>
2025-12-03 13:27:40 +0530

    Assignment 1

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# PythonLearning
-Python Learning
-my name is cpjain
+# Task 1 -  Basic Methematical Openrations
+1. First take the input  two numbers from user.
+2. Convert these number into Integer 
+3. Perform the Mathematical Operation like Addition, Subtraction, Multiplication and Division and saved the result in respective variables.
+4. Print the Results
+
+# Task 2 -  Create Personalized Greeting
+1. First take the input  first name and last name from user.
+2. Add exclamation mark after last name 
+3. Print the Greetings.
+
---
+++ Task1_Basic_Mathematical_Operation.py
@@ -0,0 +1,21 @@
+"""
+Task1_Basic_Mathematical_Operation
+"""
+# Take input from users and convert into string into integer
+
+first_number=int(input("Enter the first number: "))
+second_number=int(input("Enter the second number: "))
+
+# Perform the Mathematical operation and save results in variables
+
+addition=first_number+second_number
+subtraction=first_number-second_number
+multiplication=first_number*second_number
+division=first_number/second_number
+
+#Print the results
+
+print("Addition:",addition)
+print("Subtraction;",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
